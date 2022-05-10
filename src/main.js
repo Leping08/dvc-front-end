c66fddc23c768f02b4ecc62560cdb0927d8fcf6d
Derek Schmitt <derek@Dereks-MacBook-Pro-2.local>
2022-05-10 08:33:52 -0400

    Add GA4 to website

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import router from './router'
 import store from './store'
 import './assets/css/index.css'
 import VantageUi from 'vantage-ui'
+import VueGtag from 'vue-gtag'
 require('vantage-ui/src/lib-components/utils/content')
 
 createApp(App)
   .use(VantageUi, {
     color: 'lightBlue'
   })
+  .use(VueGtag, {
+    appName: 'DeltaV Creative',
+    pageTrackerScreenviewEnabled: true,
+    config: { id: 'G-YN1RDSJP64' }
+  }, router)
   .use(store)
   .use(router)
   .mount('#app')
